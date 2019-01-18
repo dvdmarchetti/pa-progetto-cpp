73624cad6cbd531be1ffc29430bb2126f000abdd
Davide Marchetti <dvdmarchetti663@gmail.com>
2019-01-18 16:37:19 +0100

    Added some fundamental tests

--- main.cpp
+++ main.cpp
@@ -22,19 +22,77 @@ set<T, E> filter_out(const set<T, E> s1, const set<T, E> s2, F pred) {
 void test_fondamentali() {
   set<int> s;
 
+  // Add
+  std::cout << "# Adding elements to set #" << std::endl;
   s.add(5);
   s.add(3);
   s.add(10);
   s.add(11);
 
-  std::cout << "Count: " << s.count() << std::endl;
-  std::cout << s << std::endl;
+  // Count + ostream<<
+  std::cout << "S1 = " << s << " // Count: " << s.count() << std::endl;
   assert(s.count() == 4);
 
-  s.remove(10);
-  std::cout << "Count: " << s.count() << std::endl;
-  std::cout << s << std::endl;
-  assert(s.count() == 3);
+  std::cout << std::endl;
+
+  // Copy constructor
+  std::cout << "# Copying s1 to s2 with copy constructor #" << std::endl;
+  set<int> s2(s), s3;
+  std::cout << "S2 = " << s2 << " // Count: " << s2.count() << std::endl;
+
+  std::cout << std::endl;
+
+  // Remove
+  std::cout << "# Removing element '10' to s2 #" << std::endl;
+  s2.remove(10);
+  assert(s2.count() == 3);
+  std::cout << "S2 = " << s2 << " // Count: " << s2.count() << std::endl;
+
+  std::cout << std::endl;
+
+  // Operator =
+  s3 = s2;
+  std::cout << "# Cloning object with operator= into s3 #" << std::endl;
+  assert(s3.count() == 3);
+  std::cout << "S3 = " << s3 << " // Count: " << s3.count() << std::endl;
+
+  std::cout << std::endl;
+
+  // Operator[]
+  std::cout << "# Printing the value at index 1 from a set with operator[] #" << std::endl;
+  std::cout << "S3[1] = " << s3[1] << std::endl;
+
+  std::cout << std::endl;
+
+  // Contains
+  std::cout << "# Contains method #" << std::endl;
+  std::cout << "S1 = " << s << " // Count: " << s.count() << std::endl;
+  std::cout << "Does S1 contain value 4? " << s.contains(4) << std::endl;
+
+  std::cout << std::endl;
+
+  std::cout << "# Adding value 4 to set #" << std::endl;
+  s.add(4);
+  std::cout << "S1 = " << s << " // Count: " << s.count() << std::endl;
+  std::cout << "Does S1 contain value 4? " << s.contains(4) << std::endl;
+
+  std::cout << std::endl;
+
+  // Removing all the elements
+  std::cout << "S2 = " << s2 << " // Count: " << s2.count() << std::endl;
+  std::cout << "# Removing all the elements one by one from the set s2 #" << std::endl;
+  s2.remove(11);
+  s2.remove(5);
+  s2.remove(3);
+  assert(s2.count() == 0);
+  std::cout << "S2 = " << s2 << " // Count: " << s2.count() << std::endl;
+
+  std::cout << std::endl;
+
+  // Clear
+  std::cout << "# Clearing out the set S1 #" << std::endl;
+  s.clear();
+  std::cout << "S1 = " << s << " // Count: " << s.count() << std::endl;
 }
 
 int main() {
